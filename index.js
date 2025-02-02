5b0a881168e3117cedef6e2c23b2bbc87ee9750a
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-02 14:31:49 +0530

    changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5500", 
+    origin: "http://localhost:5173", 
     credentials: true, 
   })
 );
