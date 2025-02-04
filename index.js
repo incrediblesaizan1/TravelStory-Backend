596711cea5e407f708dff243344a0f758883b8e9
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-04 20:50:48 +0530

    chnag

--- index.js
+++ index.js
@@ -26,28 +26,28 @@ mongoose
   });
 
   const allowedOrigins = [
-    "http://localhost:5173", 
-    "https://incrediblesaizan1-travel-stories.vercel.app"
+    "http://localhost:5173",
+    "https://incrediblesaizan1-travel-stories.vercel.app",
   ];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigins);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  }
-  
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 app.use(cookieParser())
+app.options("*", cors())
 
 
 app.get("/", (req, res) => {
